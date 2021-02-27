a1c2e64af6e2613bc193c8813561dc4cf1d49cb5
Admin <alam.mexuseducation@gmail.com>
2021-02-27 12:12:16 +0530

    Initial

--- app/src/main/java/com/example/mlkittranslation/TranslateActivity.kt
+++ app/src/main/java/com/example/mlkittranslation/TranslateActivity.kt
@@ -46,7 +46,6 @@ class TranslateActivity : AppCompatActivity() {
                         }
                     }
                     ?.addOnFailureListener {
-
                     }
             }
 
