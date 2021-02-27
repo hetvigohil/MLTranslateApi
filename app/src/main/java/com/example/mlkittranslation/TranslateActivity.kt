6780411c9bc3d6375f6785fa32d478e04d53d795
Hetvi <alam.mexuseducation@gmail.com>
2021-02-27 12:21:33 +0530

    Initial

--- app/src/main/java/com/example/mlkittranslation/TranslateActivity.kt
+++ app/src/main/java/com/example/mlkittranslation/TranslateActivity.kt
@@ -47,7 +47,6 @@ class TranslateActivity : AppCompatActivity() {
                     }
                     ?.addOnFailureListener {
                         
-
                     }
             }
 
